61aca5cba060cf402379dd51fd9c5c16125a49c4
Gibby <git@twoitguys>
2020-03-25 12:15:52 -0400

    Update build command

--- cmd/yagpdb/build.sh
+++ cmd/yagpdb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=$(git describe --tags)
+VERSION=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
 echo Building version $VERSION
 go build -ldflags "-X github.com/jonas747/yagpdb/common.VERSION=${VERSION}"
